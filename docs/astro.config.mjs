c88e85097dd31c2af5bdaefe57706ab8182790fd
Bernardusz <b.christian.nugroho.study@gmail.com>
2026-08-08 12:06:45 +0700

    docs: Updated the configuration to points to bernardusz.github.io/levtus/

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://bernardusz.github.io',
+  	base: '/levtus/', // Ensures all internal links use the repository prefix
 	integrations: [
 		starlight({
 			title: 'Levtus',
